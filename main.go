cf0e598c1925d1aa2515564344a05ef4742da767
goadstack <goadstack@gmail.com>
2016-05-09 19:47:30 +0200

    changed to only work with one coin currently

--- main.go
+++ main.go
@@ -7,47 +7,61 @@ import (
 
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
+	"github.com/jasonlvhit/gocron"
 	"github.com/sclevine/agouti"
 )
 
 var agoutiDriver *agouti.WebDriver
+var page *agouti.Page
+
+var storage = ""
 
 func main() {
+
+	agoutiDriver = agouti.PhantomJS()
+	agoutiDriver.Start()
+
+	page, _ = agoutiDriver.NewPage(agouti.Browser("firefox"))
+	storage = getChange("trumpcoin")
+	gocron.Every(10).Minute().Do(task)
+	gocron.Start()
+	fmt.Println(storage)
 
 	n := negroni.Classic()
 	router := mux.NewRouter()
 
 	router.HandleFunc("/change", func(w http.ResponseWriter, r *http.Request) {
-
-		currency := r.URL.Query().Get("coin")
-		w.Write([]byte(getChange(currency)))
+		w.Write([]byte(storage))
 	})
 	n.UseHandler(router)
 	n.Run(":5000")
+
+}
+
+func task() {
+	fmt.Println("I am runnning task.")
+	storage = getChange("trumpcoin")
 }
 
 func getChange(coin string) string {
-	agoutiDriver = agouti.PhantomJS()
-	agoutiDriver.Start()
-	page, err := agoutiDriver.NewPage(agouti.Browser("firefox"))
-	if err != nil {
-		fmt.Println(err)
-	}
-
 	page.Navigate("http://coinmarketcap.com/currencies/" + coin)
 
 	change := page.FirstByClass("negative_change")
 
-	isFound, _ := change.Visible()
+	isFound, err := change.Visible()
+	checkErr(err)
 
 	if isFound {
-		txt, _ := change.Text()
+		txt, err := change.Text()
+		checkErr(err)
 		return formatString(txt)
 	}
 	change = page.FirstByClass("positive_change")
-	isFound, _ = change.Visible()
+	isFound, err = change.Visible()
+	checkErr(err)
 	if isFound {
-		txt, _ := change.Text()
+		txt, err := change.Text()
+		checkErr(err)
 		return formatString(txt)
 	}
 	page.Destroy()
@@ -60,4 +74,10 @@ func formatString(str string) string {
 	str = strings.Replace(str, "%", "", -1)
 	str = strings.Replace(str, ")", "", -1)
 	return strings.Replace(str, " ", "", -1)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
 }
